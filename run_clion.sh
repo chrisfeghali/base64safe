dfe75cbe52f7840fd175dffbfca85d1bb4c5ef72
Christopher Feghali <chris@feghali.xyz>
2025-03-02 19:35:08 +0000

    cleanup

--- run_clion.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd ~/Code/blog
-ln -sf CMakeLists.txt.llvm CMakeLists.txt
-
-cd src
-ln -sf CMakeLists.txt.llvm CMakeLists.txt
-cd ..
--- src/thrift_service.thrift
+++
@@ -1,8 +0,0 @@
-namespace cpp ThriftService
-
-service ThriftService
-{
-	void ping(),
-	void hello(1:string msg),
-	oneway void async_call()
-}
